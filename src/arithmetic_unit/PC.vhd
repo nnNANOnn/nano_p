60461f6bc0ecb82ff2c8f06939546b77d6963302
arun24-cs <arun.24@cse.mrt.ac.lk>
2026-05-05 02:13:44 +0530

    Add PC entity and behavioral architecture in VHDL

---
+++ src/arithmetic_unit/PC.vhd
@@ -0,0 +1,28 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.buses.all;
+
+entity PC is
+    Port ( A : in instruction_address;
+           Res : in STD_LOGIC;
+           Clk : in STD_LOGIC;
+           M : out instruction_address);
+end PC;
+
+architecture Behavioral of PC is
+
+begin
+
+    -- 3 bit register 
+    Reg_0 : Reg
+        generic map (N => 3)
+        port map(
+                D => A,
+                Res => Res,
+                EN => '1',
+                Clk => Clk,
+                Q => M
+            );
+
+    
+end Behavioral;
