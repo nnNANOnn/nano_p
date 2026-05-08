475557cf031eae0c72f4f4a0f9b3f5e2fe98f868
Av-I-yA <avishkatheaviya@gmail.com>
2026-05-08 18:21:16 +0530

    Fixing Issues: STAGE IV

--- src/program_rom/Program_Rom.vhd
+++ src/program_rom/Program_Rom.vhd
@@ -29,27 +29,35 @@ architecture Behavioral of Program_ROM is
 
     type rom_type is array (0 to 7) of STD_LOGIC_VECTOR(11 downto 0);
 
-    -- ---- Custom assembly program from image ----
+    -- ---- Step 4 program: total of integers 1..3, result in R7 ----
     --
-    --  addr  asm              machine code (binary)         comment
-    --   0    MOVI R1, 10      10 001 000 1010               R1 <- 10
-    --   1    MOVI R2, 1       10 010 000 0001               R2 <- 1
-    --   2    NEG  R2          01 010 000 0000               R2 <- -R2
-    --   3    ADD  R1, R2      00 001 010 0000               R1 <- R1 + R2
-    --   4    JZR  R1, 7       11 001 000 0111               If R1 = 0 jump to line 7
-    --   5    JZR  R0, 3       11 000 000 0011               If R0 = 0 jump to line 3 (Loop back)
-    --   6    JZR  R0, 6       11 000 000 0110               Safety halt
-    --   7    JZR  R0, 7       11 000 000 0111               Halt (Loop to self)
+    -- Loop scheme:
+    --   R1 holds the running counter (3, 2, 1, 0).
+    --   R2 holds the constant -1 (built once via MOVI 1 + NEG).
+    --   R7 accumulates R1 each iteration.  R7 is reset to 0 by the
+    --   pushbutton on power-up, so the first ADD writes 0 + 3 = 3.
+    --
+    --  addr  asm              machine code (binary)        comment
+    --   0    MOVI R1, 3       10 001 000 0011              R1 <- 3   (counter / addend)
+    --   1    MOVI R2, 1       10 010 000 0001              R2 <- 1
+    --   2    NEG  R2          01 010 000 0000              R2 <- -1  (decrement constant)
+    --   3    ADD  R7, R1      00 111 001 0000              R7 <- R7 + R1   (loop body)
+    --   4    ADD  R1, R2      00 001 010 0000              R1 <- R1 - 1
+    --   5    JZR  R1, 7       11 001 000 0111              if R1 == 0 jump to 7 (exit)
+    --   6    JZR  R0, 3       11 000 000 0011              unconditional jump to 3 (R0 is hardwired 0)
+    --   7    JZR  R0, 7       11 000 000 0111              halt: jump-to-self
+    --
+    -- Final state: R7 = 0 + 3 + 2 + 1 = 6  =  "0110".
     --
     constant ROM_CONTENTS : rom_type := (
-        0 => "100010001010",   -- MOVI R1, 10
+        0 => "100010000011",   -- MOVI R1, 3
         1 => "100100000001",   -- MOVI R2, 1
         2 => "010100000000",   -- NEG  R2
-        3 => "000010100000",   -- ADD  R1, R2
-        4 => "110010000111",   -- JZR  R1, 7
-        5 => "110000000011",   -- JZR  R0, 3
-        6 => "110000000110",   -- JZR  R0, 6   
-        7 => "110000000111"    -- JZR  R0, 7   
+        3 => "001110010000",   -- ADD  R7, R1
+        4 => "000010100000",   -- ADD  R1, R2
+        5 => "110010000111",   -- JZR  R1, 7
+        6 => "110000000011",   -- JZR  R0, 3
+        7 => "110000000111"    -- JZR  R0, 7   (halt)
     );
 
 begin
